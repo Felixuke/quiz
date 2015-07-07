7728a9cbe59c16ef5b07ffb1eb9d7d6e68209f7d
felixuke <felixuke7@gmail.com>
2015-07-07 19:01:07 +0200

    DB hasta Autoload
    DB hasta Autoload

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -1,17 +1,35 @@
 var models = require('../models/models.js');
-//GET /quizes/question
-exports.question = function(req,res){
-	models.Quiz.findAll().success(function(quiz){
-		res.render('quizes/question',{pregunta:quiz[0].pregunta,title:'Quiz'});
-	})
+
+// Autoload - factoriza el codigo si su ruta incluye :quizId - Gestion de errores - Pregunta no existe en DB
+exports.load = function(req,res,next,quizId){
+	models.Quiz.find(quizId).then(
+		function(quiz){
+			if(quiz){
+				req.quiz = quiz;
+				next();
+			}else{ next(new Error('No existe quizId = '+quizId)); }
+		}
+	).catch(function(error) { next(error); });
 };
+
+//GET /quizes
+exports.index = function(req,res){
+	models.Quiz.findAll().then(function(quizes){
+		res.render('quizes/index',{quizes:quizes,title:'Quizes'});
+	}).catch(function(error) { next(error); });
+};
+
+//GET /quizes/question
+exports.show = function(req,res){
+	res.render('quizes/show',{quiz:req.quiz,title:'Quiz'});
+};
+
 //GET /quizes/answer
 exports.answer = function(req,res){
-	models.Quiz.findAll().success(function(quiz){
-		if(req.query.respuesta===quiz[0].respuesta){
-			res.render('quizes/answer',{respuesta:'Correcto',title:'Quiz'});
-		}else{
-			res.render('quizes/answer',{respuesta:'Incorrecto',title:'Quiz'});
-		}
-	})
+	var resultado='Incorrecto';
+	if(req.query.respuesta===req.quiz.respuesta){
+		resultado='Correcto';
+	}
+	res.render('quizes/answer',{quiz:req.quiz,respuesta:resultado,title:'Quiz'});
+	
 };
--- models/models.js
+++ models/models.js
@@ -31,19 +31,19 @@ var sequelize = new Sequelize(DB_name,user,pwd,
 var quiz_path ;
 var Quiz = sequelize.import(path.join(__dirname,'quiz'));
 // Exportamos definicion de tabla Quiz
-
-
-
 exports.Quiz = Quiz;
 
 // Crea e inicializa la tabla Quiz
-sequelize.sync().success(function(){
-	Quiz.count().success(function(count){
+sequelize.sync().then(function(){
+	Quiz.count().then(function(count){
 		if(count===0){
 			Quiz.create({pregunta:'¿Capital de Italia?',
 						 respuesta:'Roma'
+						});
+			Quiz.create({pregunta:'¿Capital de Portugal?',
+						 respuesta:'Lisboa'
 						})
-			.success(function(){console.log('Base de datos inicializada')});
+			.then(function(){console.log('Base de datos inicializada')});
 		};
 	});
 });
